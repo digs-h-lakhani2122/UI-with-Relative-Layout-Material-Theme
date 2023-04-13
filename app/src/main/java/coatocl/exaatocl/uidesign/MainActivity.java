d172608c117617f0cc353a191baa51b3fc61ec5a
digs-h-lakhani2122 <digallakhani21@gmail.com>
2023-04-13 12:18:59 +0530

    UI-with-Relative-Layout-Material

---
+++ app/src/main/java/coatocl/exaatocl/uidesign/MainActivity.java
@@ -0,0 +1,21 @@
+package coatocl.exaatocl.uidesign;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+import android.view.Window;
+import android.view.WindowManager;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+//        it will hide the title
+//        getWindow().setFlags(WindowManager.LayoutParams.FLAG_FULLSCREEN,WindowManager.LayoutParams.FLAG_FULLSCREEN);
+//        setContentView(R.layout.activity_main);
+
+    }
+}
